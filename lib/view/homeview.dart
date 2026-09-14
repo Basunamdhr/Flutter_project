3d073cd391f01bb25e454c0a8bddc77adb9022d2
Basunamdhr <basunamdhr45@gmail.com>
2026-09-14 21:28:40 +0545

    scaffold

---
+++ lib/view/homeview.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homeview extends StatefulWidget {
+  const new({super.key});
+
+  @override
+  State<Homeview> createState() => _HomeviewState();
+}
+
+class _HomeviewState extends State<Homeview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Home view!'),
+      ),
+    );
+  }
+}
